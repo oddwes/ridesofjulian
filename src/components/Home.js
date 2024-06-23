9529cd64406c564d9b595f7370c7890cfddb1121
oddwes <oddwes3.0@gmail.com>
2024-06-23 15:18:24 -0700

    Adds some stats to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,53 @@
-import { getAthlete, isLoggedIn } from "../utils/StravaUtil"
+import { getAtheleteStats, getAthlete, getAthleteActivities, isLoggedIn } from "../utils/StravaUtil"
 import { useEffect, useState } from "react"
 
 import { Navigate } from "react-router-dom"
 
 const Home = () => {
   const [athlete, setAthlete] = useState()
+  const [athleteActivities, setAthleteActivities] = useState([{}])
 
   const loadAthlete = async () => {
     const response = await getAthlete()
     setAthlete(response)
   }
 
+  const loadAthleteActivities = async () => {
+    const response = await getAthleteActivities()
+    setAthleteActivities(response)
+    console.log(response)
+  }
+
+  const getTotalTime = () => {
+    let totalRideTime = athleteActivities.map((activity) => activity.elapsed_time).reduce((partialSum, a) => partialSum + a, 0)
+    return totalRideTime / 3600
+  }
+
+  const getTotalDistance = () => {
+    let totalRideDistance = athleteActivities.map((activity) => activity.distance).reduce((partialSum, a) => partialSum + a, 0)
+    return totalRideDistance / 1000
+  }
+
   useEffect(() => {
     loadAthlete()
+    loadAthleteActivities()
   }, [])
 
   return (
     <>
-      <h1>THE BEST STRAVA DASHBOARD EVER</h1>
+      <h1>HOMEPAGE</h1>
       <h2>Hello {athlete?.firstname}</h2>
+      <h2>Total ride time this year: {getTotalTime()} hours</h2>
+      <h2>Total ride time this year: {getTotalDistance()} KMs</h2>
+      {
+        athleteActivities.map((activity) => {
+          return (
+            <pre key={activity.id}>
+              {JSON.stringify(activity, null, 0)}
+            </pre>
+          )
+        })
+      }
       {!isLoggedIn() && <Navigate replace to="/login" />}
     </>
   )
--- src/utils/StravaUtil.js
+++ src/utils/StravaUtil.js
@@ -24,14 +24,45 @@ export const getAccessToken = async (authCode) => {
 }
 
 export const isLoggedIn = () => {
-  return !!localStorage.getItem(ACCESS_TOKEN_KEY)
+  return (!!localStorage.getItem(ACCESS_TOKEN_KEY)) && (localStorage.getItem(TOKEN_EXPIRY_KEY) > Date.now()/1000)
 }
 
 export const getAthlete = async () => {
+  return await stravaApiV3Get('https://www.strava.com/api/v3/athlete')
+}
+
+export const getAthleteStats = async () => {
+  const athlete = await getAthlete()
+  return await stravaApiV3Get(`https://www.strava.com/api/v3/athletes/${athlete.id}/stats`)
+}
+
+export const getAthleteActivities = async (page = 1) => {
+  const perPage = 50
+  const firstDayOfYear = new Date(new Date().getFullYear(), 0, 1).getTime()/1000
+  const activities = await stravaApiV3Get(
+    'https://www.strava.com/api/v3/athlete/activities',
+    {
+      after: firstDayOfYear,
+      page: page,
+      per_page: perPage,
+    }
+  )
+  if(activities.length === perPage) {
+    const nextPage = await getAthleteActivities(page + 1)
+    return activities.concat(nextPage)
+  } else {
+    return activities
+  }
+}
+
+const stravaApiV3Get = async (url, params = {}) => {
   try {
     const response = await axios.get(
-      'https://www.strava.com/api/v3/athlete',
-      { headers: { Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN_KEY)}` } }
+      url,
+      {
+        headers: { Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN_KEY)}` },
+        params: params
+      }
     );
     return response.data;
   } catch (error) {
