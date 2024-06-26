4ecb22eb4010d8dae933951770cea24ec7d25f52
oddwes <oddwes3.0@gmail.com>
2024-06-26 14:31:13 -0700

    Login button styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import '../styling/strava.css';
 
+import { Col, Row } from 'react-bootstrap';
 import { isLoggedIn, login } from "../utils/StravaUtil"
 
 import { Navigate } from 'react-router-dom';
@@ -7,10 +8,12 @@ import strava_logo from "../assets/images/strava-logo.svg"
 
 const Login = () => {
   const loginButton =
-    <button className="strava-button" onClick={login}>
-      <img src={strava_logo} width="50" height="50" alt="strava logo"/>
-      <span style={{fontWeight: "bold"}}>Login</span>
-    </button>
+    <Row className='justify-content-md-center'>
+      <button className="strava-button" onClick={login}>
+        <img src={strava_logo} width="50" height="50" alt="strava logo"/>
+        <span style={{fontWeight: "bold"}}> Login</span>
+      </button>
+    </Row>
 
   return (
     isLoggedIn() ? <Navigate replace to="/" /> : loginButton
