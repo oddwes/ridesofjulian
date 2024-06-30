8326c9b524e7879249b732c8255834781d1460a3
oddwes <oddwes777@gmail.com>
2024-06-29 23:49:37 -0700

    Updates

--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-const Header = () => {
-  return (
-    <h1 style={{padding: '20px', textAlign: 'center', color: 'white', backgroundColor: '#FC5201'}}>RIDESOFJULIAN</h1>
-  )
-}
-
-export default Header
