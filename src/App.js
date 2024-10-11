d6e2a7a9ee5255e5f7a88303f42d7c84ce12309e
oddwes <oddwes777@gmail.com>
2024-10-10 21:15:40 -0400

    trail is now a solid line

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { BikeTrails } from "./components/BikeTrails"
 
 function App() {
   return (
     <div className="App">
+      <BikeTrails />
       <Header />
       <RouterProvider router={router} />
     </div>
