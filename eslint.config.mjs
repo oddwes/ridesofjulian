2d5eefaf2e1b7203966dd1b930f60fb0dfbe73a9
oddwes <oddwes777@gmail.com>
2024-11-03 21:24:45 -0500

    performance improvement for background scrolling bike

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,28 +2,30 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import prettier from "eslint-plugin-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import { fixupPluginRules } from "@eslint/compat";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: { globals: {...globals.node, ...globals.browser} },
-  },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    plugins: {
-      prettier,
-    },
-    rules: {
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-    },
-    settings: {
-      react: {
-        version: "detect"
-      }
+    pluginJs.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        languageOptions: { globals: {...globals.node, ...globals.browser} },
+        plugins: {
+            prettier,
+            "react-hooks": fixupPluginRules(reactHooks),
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        rules: {
+            "react/jsx-uses-react": "off",
+            "react/react-in-jsx-scope": "off",
+            "react/prop-types": "off",
+            "react-hooks/exhaustive-deps": "warn",
+        },
     }
-  }
 ];
