4767b50dd66c314008787132dc7140d9ccb32298
oddwes <oddwes3.0@gmail.com>
2025-05-30 22:29:16 -0700

    update node

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,7 @@ import { BikeTrails } from './BikeTrails';
 
 const Home = () => {
   const yearOptions = [
+    { value: 2025, label: 2025 },
     { value: 2024, label: 2024 },
     { value: 2023, label: 2023 },
     { value: 2022, label: 2022 },
