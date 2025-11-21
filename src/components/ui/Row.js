bae6136cfa0c1b6d3fd9b2e756762428bc4c454b
oddwes <oddwes777@gmail.com>
2025-11-21 11:58:57 +0700

    styling

--- src/components/ui/Row.js
+++ src/components/ui/Row.js
@@ -1,7 +1,7 @@
 const Row = ({ header, columns, className = "" }) => {
   return (
-    <div className={`flex items-center mx-4 w-full ${className}`}>
-      <div className="min-w-34 my-2">
+    <div className={`flex items-center mx-4 w-full h-full grow ${className}`}>
+      <div className="my-2 mr-4">
         {header}
       </div>
       <div className="grid grid-cols-7 justify-between w-11/12 h-full flex-1">
